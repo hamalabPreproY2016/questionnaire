9cab4d2496c7053edffcd23d738cc9ec6a26006f
ragrog <ragrog216@outlook.jp>
2017-05-19 15:58:20 +0900

    ページ内で直接動画を再生できるようにした

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var isRunning = false;
 var State = 0;
 var timer;
 var expTime;
+var video;
 dateFormat = {
   fmt : {
     "yyyy": function(date) { return date.getFullYear() + ''; },
@@ -25,7 +26,7 @@ dateFormat = {
 var countup = function(){
 	rt = Date.now() - StartTime;
 	$("#elapsedTime").text("経過時間 : " + parseInt(rt / 1000) + "秒" );
-	if (rt > expTime){
+	if (rt / 1000 > expTime){
 		State = changeState(1);
 	}
 	$(".list").append('<td>　</td>');
@@ -39,7 +40,6 @@ var countup = function(){
 	last.opentip((rt / 1000).toString(), { delay: 0 });
 	$(".scroll").scrollLeft($(".list").width());
 	last.on('click', function(){
-		console.log("hi");
 		if ($(this).attr("class") === "is-angry")
 		{
 			$(this).removeClass("is-angry");
@@ -54,17 +54,19 @@ var changeState = function(s){
 		switch(s){
 			case 0: // 評価開始
 				StartTime = Date.now();
-				expTime = (parseInt($("#minutes").val()) * 60 + parseInt($("#seconds").val()) ) * 1000;
+				video[0].play();
 				timer = setInterval(countup, 1000);
 				$("#Start").text("評価停止");
 				s = 1;
 			break;
 			case 1: // 評価停止
 				clearInterval(timer);
+				video[0].pause();
 				$("#Start").text("評価再開");
 				s = 2;
 			break;
 			case 2: // 評価再開
+				video[0].play();
 				// 開始時間に再開を押すまでの時間をプラスする
 				StartTime = Date.now() - $(".list td:last").data("time");
 				timer = setInterval(countup, 1000);
@@ -72,6 +74,8 @@ var changeState = function(s){
 				s = 1;
 			break;
 			case -1: // 初期化
+				video[0].pause();
+				video.prop('currentTime', 0);
 				clearInterval(timer);
 				$("#Start").text("評価開始");
 				$(".list td").remove();
@@ -80,15 +84,25 @@ var changeState = function(s){
 		}
 		return s;
 }
+var updateControllerUI = function(enabled){
+	$('#Init').prop('disabled', !enabled);
+	$('#Start').prop('disabled', !enabled);
+	$('.angry-toggle').prop('disabled', !enabled);
+}
+var getFileName = function(){
+	var fileName = $("#video-file")[0].files[0].name.replace(/^.*[\\\/]/, '');
+	fileName =  fileName.substr( 0, fileName.length-4 ); 
+	var type =  $('input[name="type"]:checked').val();
+	fileName = fileName + "-" + type + "-" + $('#author').val() + ".csv";
+	return fileName;
+}
 $(function(){
-	$("#fileName").val(dateFormat.format(new Date(), 'yyyy-MM-dd-hh-mm-ss'))
+	$("#fileName").val(dateFormat.format(new Date(), 'yyyy-MM-dd-hh-mm-ss'));
 	$(".angry-toggle").on('keydown', function(){
 		isAngry = true;
-		console.log("push");
 	});
 	$(".angry-toggle").on('keyup', function(){
 		isAngry = false;
-		console.log("up");
 	});
 	$("#Start").on('click', function()
 	{
@@ -98,8 +112,6 @@ $(function(){
 		State = changeState(-1);
 	});
 	$("#download").on('click', function(){
-		var type =  $('input[name="type"]:checked').val();
-		var fileName = $("#fileName").val() + "-" + type + ".csv";
 		var csv = "";
 		$(".list td").each(
 			function(index, element){
@@ -110,7 +122,7 @@ $(function(){
 				csv += time.toString() + "," + (isAngry? 1 : 0) + "\n";
 			}
 		);
-	console.log(fileName);
+		var fileName = getFileName();
 		$("#download").attr("download", fileName);
 		var blob = new Blob([ csv ], { "type" : "text/csv" });
 		if (window.navigator.msSaveBlob) { 
@@ -121,6 +133,37 @@ $(function(){
 		} else {
 			document.getElementById("download").href = window.URL.createObjectURL(blob);
 		}
+	});
+	$("#video-file").on('change', function(e){
+        var file = e.target.files[0];
+        if (!file) {
+            return;
+        };
+        var url = window.URL.createObjectURL(file);
+		video.attr('src', url);
+		video.on('loadeddata', function(){
+				expTime = video[0].duration;
+			var textTime = (("0"+parseInt(expTime / 60)).slice(-2) + " : " + ("0"+parseInt(expTime % 60)).slice(-2) );
+			$("#time").text(textTime);
+		});
+		$("#fileName").text(getFileName());
+		updateControllerUI(true);
+	});
+	video = $("#player");
+	video.on('play', function(){
+		if (State != 1){
+			State = changeState(0);
+		}
+	});
+	video.on('pause', function(){
+		if (State != 2){
+			State = changeState(1);
+		}
+	});
+	$("#author").on('input', function(){
+		$('#video-file').prop('disabled', $(this).val() === "");
 	})
+	updateControllerUI(false);
+	$('#video-file').prop('disabled', true);
 
 });
